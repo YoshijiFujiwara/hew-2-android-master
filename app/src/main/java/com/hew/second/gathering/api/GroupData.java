71a575f434438c68a33137f4c9c4b7310fb3655d
yoshiji <yfbiology2@gmail.com>
2019-02-12 13:16:35 +0900

    APIクラス名の改修

--- app/src/main/java/com/hew/second/gathering/api/GroupData.java
+++
@@ -1,7 +0,0 @@
-package com.hew.second.gathering.api;
-
-import java.util.List;
-
-public class GroupData {
-    public List<GroupInfo> data;
-}
---
+++ app/src/main/java/com/hew/second/gathering/api/MemberList.java
@@ -0,0 +1,10 @@
+package com.hew.second.gathering.api;
+
+import java.util.List;
+
+/**
+ * ユーザー一覧
+ */
+public class MemberList {
+    public List<Member> data;
+}
