948f6d58ec22b174d46798208c86bd5400e31e04
kazuhou <hozuka.zero3zer8@gmail.com>
2019-02-20 18:00:12 +0900

    セッション詳細新規ファイル追加

--- app/src/main/java/com/hew/second/gathering/fragments/FragmentEventFinish.java
+++ app/src/main/java/com/hew/second/gathering/fragments/FragmentEventFinish.java
@@ -1,4 +1,23 @@
 package com.hew.second.gathering.fragments;
 
-public class FragmentEventFinish {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentEventFinish extends Fragment {
+
+    public FragmentEventFinish() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view =
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
--- app/src/main/java/com/hew/second/gathering/fragments/FragmentEventPrecessMain.java
+++ app/src/main/java/com/hew/second/gathering/fragments/FragmentEventPrecessMain.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,22 @@ public class FragmentEventPrecessMain extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_event_process_main,container,false);
+
+        if (getFragmentManager() != null) {
+
+//          FragmentManagerからFragmentTransactionを作成
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+//           FragmentにActivityに登録
+//            fragmentTransaction.replace(R.id.sip_container,);
+//          バックスタックに登録
+            fragmentTransaction.addToBackStack(null);
+//           上記の変更を登録
+            fragmentTransaction.commit();
+
+        }
+
 
 
         return view;
