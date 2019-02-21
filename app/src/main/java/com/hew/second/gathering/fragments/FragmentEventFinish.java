695ee029f1647e8d6aa0e351c6c67395eac31a28
kazuhou <hozuka.zero3zer8@gmail.com>
2019-02-21 11:18:03 +0900

    とりあえずコミット

--- app/src/main/java/com/hew/second/gathering/fragments/FragmentEventFinish.java
+++ app/src/main/java/com/hew/second/gathering/fragments/FragmentEventFinish.java
@@ -17,7 +17,7 @@ public class FragmentEventFinish extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view =
+        
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
