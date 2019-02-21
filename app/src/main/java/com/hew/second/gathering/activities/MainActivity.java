34068bbd14898c1db8e12398055cbdf98e758ecc
Tsukasa Murata <stepfootprint@gmail.com>
2019-02-22 03:08:24 +0900

    現在地取得のランタイムパーミッション追加

--- app/src/main/java/com/hew/second/gathering/activities/MainActivity.java
+++ app/src/main/java/com/hew/second/gathering/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
