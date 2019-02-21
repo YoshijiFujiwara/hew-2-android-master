cee6e926e9ea750df620496f60eabfc9331ac863
hsmkyo <hsmkyo2591ys@yahoo.co.jp>
2019-02-21 21:32:02 +0900

    デフォルト追加、編集画面の表示

--- app/src/main/java/com/hew/second/gathering/fragments/AddDefaultSettingFragment.java
+++ app/src/main/java/com/hew/second/gathering/fragments/AddDefaultSettingFragment.java
@@ -17,15 +17,25 @@ import android.widget.Spinner;
 import com.hew.second.gathering.LogUtil;
 import com.hew.second.gathering.LoginUser;
 import com.hew.second.gathering.R;
+import com.hew.second.gathering.SearchArgs;
 import com.hew.second.gathering.activities.AddDefaultSettingActivity;
 import com.hew.second.gathering.activities.EditGroupActivity;
 import com.hew.second.gathering.activities.LoginActivity;
 import com.hew.second.gathering.api.ApiService;
 import com.hew.second.gathering.api.DefaultSetting;
 import com.hew.second.gathering.api.DefaultSettingDetail;
+import com.hew.second.gathering.api.DefaultSettingList;
+import com.hew.second.gathering.api.Group;
+import com.hew.second.gathering.api.GroupList;
 import com.hew.second.gathering.api.Util;
+import com.hew.second.gathering.hotpepper.Genre;
+import com.hew.second.gathering.hotpepper.GenreResult;
+import com.hew.second.gathering.hotpepper.HpHttp;
+import com.hew.second.gathering.views.adapters.GroupAdapter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -41,6 +51,10 @@ import static com.hew.second.gathering.activities.BaseActivity.SNACK_MESSAGE;
 public class AddDefaultSettingFragment extends BaseFragment {
     private static final String MESSAGE = "message";
     int defaultSettingId = -1;
+    ArrayList<GroupAdapter.Data> ar = new ArrayList<GroupAdapter.Data>();
+    GroupAdapter adapter = null;
+    private List<Group> groupList = new ArrayList<>();
+    private Spinner spinner = null;
 
     public static AddDefaultSettingFragment newInstance() {
         return new AddDefaultSettingFragment();
@@ -100,6 +114,36 @@ public class AddDefaultSettingFragment extends BaseFragment {
 //        // どれも選択されていなければgetCheckedRadioButtonIdは-1が返ってくる
 //        int checkedId = mRadioGroup.getCheckedRadioButtonId();
 
+        ApiService service = Util.getService();
+        HashMap<String, String> body = new HashMap<>();
+        Observable<GroupList> token = service.getGroupList(LoginUser.getToken());
+        cd.add(token.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(
+                        list -> {
+                            if (activity != null) {
+                                groupList = new ArrayList<>(list.data);
+                                ArrayList<String> data = new ArrayList<>();
+                                for (Group g : groupList) {
+                                    data.add(g.name);
+                                }
+                                spinner = activity.findViewById(R.id.group_spinner);
+                                ArrayAdapter adapter =
+                                        new ArrayAdapter(activity, android.R.layout.simple_spinner_item, data.toArray(new String[0]));
+                                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                spinner.setAdapter(adapter);
+                            }
+                        },  // 成功時
+                        throwable -> {
+                            Log.d("api", "API取得エラー：" + LogUtil.getLog() + throwable.toString());
+                            if (activity != null && !cd.isDisposed() && throwable instanceof HttpException && ((HttpException) throwable).code() == 401) {
+                                Intent intent = new Intent(activity.getApplication(), LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                ));
+
          Button saveButton = activity.findViewById(R.id.save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -150,15 +194,16 @@ public class AddDefaultSettingFragment extends BaseFragment {
 
     public void createDefault() {
         ApiService service = Util.getService();
+
         EditText defaultName = activity.findViewById(R.id.default_input);
         EditText startTime = activity.findViewById(R.id.start_time);
-//        Spinner spinner = activity.findViewById(R.id.group_spinner);
+        Spinner spinner = activity.findViewById(R.id.group_spinner);
 
-//        textView.setText(item);
         HashMap<String, String> body = new HashMap<>();
+
         body.put("name", defaultName.getText().toString());
         body.put("timer", startTime.getText().toString());
-//        body.put("group", spinner.getSelectedItem().toString());
+        body.put("group", spinner.getSelectedItem().toString());
 
         Observable<DefaultSettingDetail> token = service.createDefaultSetting(LoginUser.getToken(), body);
         cd.add(token.subscribeOn(Schedulers.io())
