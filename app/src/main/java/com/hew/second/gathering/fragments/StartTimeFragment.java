918ec07d7bbfbdd7110765f7531d91255c0bf302
kazuhou <hozuka.zero3zer8@gmail.com>
2019-02-25 01:20:10 +0900

    開始日時終了日時設定処理

--- app/src/main/java/com/hew/second/gathering/fragments/StartTimeFragment.java
+++ app/src/main/java/com/hew/second/gathering/fragments/StartTimeFragment.java
@@ -1,12 +1,17 @@
 package com.hew.second.gathering.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.hew.second.gathering.R;
 
@@ -16,14 +21,6 @@ import java.util.Calendar;
 //　開始時刻設定
 public class StartTimeFragment extends SessionBaseFragment {
 
-    Calendar calendar;
-    int startYear;
-    int startMonth;
-    int startDay;
-    int endYear;
-    int endMonth;
-    int getEndMonth;
-
     public static StartTimeFragment newInstance() {
         Bundle args = new Bundle();
         StartTimeFragment fragment = new StartTimeFragment();
@@ -31,73 +28,111 @@ public class StartTimeFragment extends SessionBaseFragment {
         return fragment;
     }
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//
-
-
         view = inflater.inflate(R.layout.fragment_starttime, container, false);
+        getActivity().setTitle("開始日時・終了日時 設定");
         return view;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+//        .setTile()
+
+        Calendar calendar = Calendar.getInstance();
+//      現在 年 月 日 時 分
+        int year = calendar.get(Calendar.YEAR);
+        int monthOfYear = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
         TextView startDateText = getActivity().findViewById(R.id.start_date);
         TextView startTimeText = getActivity().findViewById(R.id.start_timer);
         TextView endDateText = getActivity().findViewById(R.id.end_date);
         TextView endTimeText = getActivity().findViewById(R.id.end_timer);
 
-//       開始時刻セット・フォーマット
-        Calendar cl = Calendar.getInstance();
-        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy年MM月dd日");
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy年MM月dd日E曜日");
         SimpleDateFormat sdfTime = new SimpleDateFormat("HH時mm分");
-//        開始日時時刻・終了時刻が設定されてない場合の初期値
-        startDateText.setText(sdfDate.format(cl.getTime()));
-        startTimeText.setText(sdfTime.format(cl.getTime()));
-        endDateText.setText(sdfDate.format(cl.getTime()));
+//        開始　終了設定されないとき
+        startDateText.setText(sdfDate.format(calendar.getTime()));
+        startTimeText.setText(sdfTime.format(calendar.getTime()));
+        endDateText.setText(sdfDate.format(calendar.getTime()));
         endTimeText.setText(sdfTime.format(addHour(1).getTime()));
 
         if (savedInstanceState == null) {
+//            第1 context 第2 日付が選択された時のコールバック 第3 年の値 第4 月の値 第5 日の値
+            DatePickerDialog startDate = new DatePickerDialog(getActivity(),new DateSetHandler(startDateText),year,monthOfYear,dayOfMonth);
+            TimePickerDialog startTime = new TimePickerDialog(getActivity(),new DateSetHandler(startTimeText),hourOfDay,minute,true);
+            DatePickerDialog endDate = new DatePickerDialog(getActivity(),new DateSetHandler(endDateText),year,monthOfYear,dayOfMonth);
+            TimePickerDialog endTime = new TimePickerDialog(getActivity(),new DateSetHandler(endTimeText),hourOfDay,minute,true);
 
-            DatePickerDialogFragment datePicker = new DatePickerDialogFragment();
-            TimePickerDialogFragment timePicker = new TimePickerDialogFragment();
-
+//              開始日付のTextViewがクリックされた時
             startDateText.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    datePicker.show(getFragmentManager(), "startDatePicker");
-
+                    startDate.show();
                 }
             });
-
+//               開始時間のTextViewがクリックされた時
             startTimeText.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    timePicker.show(getFragmentManager(),"startTimePicker");
+
+                    startTime.show();
                 }
             });
-
+//              終了日付のTextViewがクリックされた時
             endDateText.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    datePicker.show(getFragmentManager(),"endDatePicker");
+                    endDate.show();
                 }
             });
-
+//              終了時間のTextViewがクリックされた時
             endTimeText.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    timePicker.show(getFragmentManager(),"endDatePicker");
+                    endTime.show();
                 }
             });
+        }
+    }
+//      日付・時刻選択された時TextViewに格納
+    private class DateSetHandler implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
+        private TextView textView;
+        private Calendar calendar = Calendar.getInstance();
 
+        public DateSetHandler(TextView textView) {
 
+            this.textView = textView;
 
+        }
+//      日付が選択された時の処理
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+            CharSequence text = DateFormat.format("yyyy/MM/dd,E",calendar);
+            textView.setText(text);
+
+        }
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+            calendar.set(Calendar.MINUTE,minute);
+
+            CharSequence text = DateFormat.format("kk:mm",calendar);
+            textView.setText(text);
         }
     }
 
@@ -110,7 +145,7 @@ public class StartTimeFragment extends SessionBaseFragment {
     public static Calendar addHour(int addHour){
         return add(null,0,0,0,addHour,0,0,0);
     }
-     /**
+    /**
      * 各時間フィールドに設定する数量が0の場合は、現在の値が設定されます。
      * java.util.GregorianCalendarの内部処理では以下の分岐を行っている。
      *     if (amount == 0) {
@@ -119,29 +154,17 @@ public class StartTimeFragment extends SessionBaseFragment {
      *
      * @param cal 日付時刻の指定があればセットする。
      *     nullの場合、現在の日付時刻で新しいCalendarインスタンスを生成する。
-     * @param addYear 加算・減算する年数
-     * @param addMonth 加算・減算する月数
-     * @param addDate 加算・減算する日数
      * @param addHour 加算・減算する時間
-     * @param addMinute 加算・減算する分
-     * @param addSecond 加算・減算する秒
-     * @param addMillisecond 加算・減算するミリ秒
-     * @return    計算後の Calendar インスタンス。
      */
     public static Calendar add(Calendar cal,
-                               int addYear,int addMonth,int addDate,
+                               int addYera,int addMonth,int addDate,
                                int addHour,int addMinute,int addSecond,
                                int addMillisecond){
         if (cal == null) {
             cal = Calendar.getInstance();
         }
-        cal.add(Calendar.YEAR, addYear);
-        cal.add(Calendar.MONTH, addMonth);
-        cal.add(Calendar.DATE, addDate);
         cal.add(Calendar.HOUR_OF_DAY, addHour);
-        cal.add(Calendar.MINUTE, addMinute);
-        cal.add(Calendar.SECOND, addSecond);
-        cal.add(Calendar.MILLISECOND, addMillisecond);
         return cal;
     }
+
 }
