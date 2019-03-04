e6607652223f2150a6abcd31b86df1ba956f4108
Tsukasa Murata <stepfootprint@gmail.com>
2019-03-05 06:23:13 +0900

    イベントリスト修正

--- app/src/main/java/com/hew/second/gathering/views/adapters/SessionAdapter.java
+++ app/src/main/java/com/hew/second/gathering/views/adapters/SessionAdapter.java
@@ -9,68 +9,73 @@ import android.widget.TextView;
 
 import com.hew.second.gathering.R;
 import com.hew.second.gathering.api.Session;
-import com.hew.second.gathering.api.SessionUser;
+import com.hew.second.gathering.hotpepper.Shop;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class SessionAdapter extends BaseAdapter {
 
-    private List<Session> list;
-    private int usersList;
+    protected List<Session> list;
+    protected List<Shop> shopList = null;
 
-    public static class ViewHolder {
-        TextView sessionName;
-        TextView sessionDate;
-        TextView sessionPeople;
-        ImageView sessionImage;
-        String startDate;
-        String emdDate;
+    static public class ViewHolder {
+        TextView title;
+        TextView shop_name;
+        TextView time;
+        TextView count_member;
+        ImageView imageView;
     }
-    public SessionAdapter(Session[] data) {
-        list =  Arrays.asList(data);
+
+    protected SessionAdapter() {
+        list = null;
     }
-    public SessionAdapter(ArrayList<Session> data) {
-        list = new ArrayList<>(data);
+
+    public SessionAdapter(ArrayList<Session> names, List<Shop> shops) {
+        list = new ArrayList<>(names);
+        shopList = new ArrayList<>(shops);
     }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        SessionAdapter.ViewHolder holder;
+        ViewHolder holder;
+
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.fragment_session_cell,parent,false);
-            holder = new SessionAdapter.ViewHolder();
-            holder.sessionName = convertView.findViewById(R.id.session_site);
-            holder.sessionDate = convertView.findViewById(R.id.session_date);
-            holder.sessionPeople = convertView.findViewById(R.id.session_people);
-            holder.sessionImage = convertView.findViewById(R.id.session_image_her);
-
+                    .inflate(R.layout.session_cell, parent, false);
+            holder = new ViewHolder();
+            holder.title = convertView.findViewById(R.id.title);
+            holder.shop_name = convertView.findViewById(R.id.shop_name);
+            holder.time = convertView.findViewById(R.id.time);
+            holder.count_member = convertView.findViewById(R.id.count_member);
+            holder.imageView = convertView.findViewById(R.id.imageView_shop);
             convertView.setTag(holder);
         } else {
-
-            holder = (SessionAdapter.ViewHolder)convertView.getTag();
-
-        }
-        holder.sessionName.setText(list.get(position).name);
-        holder.sessionDate.setText(list.get(position).start_time);
-        holder.sessionPeople.setText(String.valueOf(list.get(position).users.size()) + "人");
-        ArrayList<SessionUser> sessionUserList = (ArrayList<SessionUser>) list.get(position).users;
-
-        for (int i = 0; i < list.get(position).users.size(); i++) {
-            if (list.get(position).users.get(i).paid != 1) {
-                holder.sessionImage.setImageResource(R.drawable.ic_warning);
-            }
-
+            holder = (ViewHolder) convertView.getTag();
         }
 
+        holder.title.setText(list.get(position).name);
+        holder.shop_name.setText(shopList.get(position).name);
+        if(list.get(position).start_time == null){
+            holder.time.setText("未定");
+        }else{
+            holder.time.setText(list.get(position).start_time + "〜");
+        }
+        holder.count_member.setText(list.get(position).users.size() + 1 + "名");
 
+        if (shopList != null) {
+            Picasso.get()
+                    .load(shopList.get(position).photo.pc.l)
+                    .fit()
+                    .centerInside()
+                    .into(holder.imageView);
+        }
         return convertView;
     }
 
     @Override
-
     public int getCount() {
         return list.size();
     }
@@ -80,10 +85,16 @@ public class SessionAdapter extends BaseAdapter {
         return position;
     }
 
+    public List<Session> getList() {
+        return list;
+    }
+
+    public void clear() {
+        list.clear();
+    }
+
     @Override
     public long getItemId(int position) {
         return position;
     }
-
-
 }
